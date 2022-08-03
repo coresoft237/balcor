ed10388d1188cd32b88784df52c193df842e3f23
esakimedia <apachecordovax@gmail.com>
2022-08-03 12:39:28 +0200

    petite modif

--- code.php
+++ code.php
@@ -65,7 +65,7 @@ require 'elements/header.php';
                         <?php endif ?>
                         
                         <?php if (isset($single_compte->credit_solde) && in_array($single_compte->credit_solde, array_values($comptes_complet_pas_credit_solde))): ?>
-                            <td style="padding: 0; background-color: #dc3545;"><button type="button" style="border: 0; border-radius: 0; margin: 0;  width: 100px;" class="btn btn-danger" data-bs-toggle="popover" title="Attention" data-bs-content="Ce compte ne doit pas avoir un montant a cette position"><?= $single_compte->credit_solde ?></button></td>
+                            <td style="padding: 0; background-color: #dc3545;"><button type="button" style="border: 0; border-radius: 0; margin: 0;  width: 100px;" class="btn btn-danger" data-bs-toggle="popover" title="Conformite solde" data-bs-content="Ce compte ne doit pas avoir un montant a cette position"><?= $single_compte->credit_solde ?></button></td>
                         <?php else: ?>
                             <!-- style="background-color: #20c997;" -->
                             <td><?= $single_compte->credit_solde ?></td>
@@ -77,80 +77,71 @@ require 'elements/header.php';
     </div>
 
     <div class="col-sm-3 col-md-3 col-lg-3 col-xl-3">
-        <?php foreach($comptes_complet_debit_mou as $compte_complet_debit_mou): ?>
-            <?php foreach($comptes_complet_credit_mou as $compte_complet_credit_mou): ?>
-                <?php if($compte_complet_debit_mou === $compte_complet_credit_mou): ?>
-                    <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-                        <div class="card-header">
-                            <h6>Conformite Amortissements</h6>
-                        </div>
-
-                        <div class="card-body">
-                            <p>Correcte</p>
-                        </div>
-                    </div>
-                <?php else: ?>
-                    <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
-                        <div class="card-header">
-                            <h6>Conformite Amortissements</h6>
-                        </div>
-
-                        <div class="card-body">
-                            <p>Incorrecte</p>
-                        </div>
-                    </div>
-                <?php endif ?>
-            <?php endforeach ?>
-        <?php endforeach ?>
-
-        <?php foreach($comptes_complet_debit_mou_69 as $compte_complet_debit_mou): ?>
-            <?php foreach($comptes_complet_credit_mou_29 as $compte_complet_credit_mou): ?>
-                <?php if($compte_complet_debit_mou === $compte_complet_credit_mou): ?>
-                    <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-                        <div class="card-header">
-                            <h6>Conformite Provisions</h6>
-                        </div>
-
-                        <div class="card-body">
-                            <p>Correcte</p>
-                        </div>
-                    </div>
-                <?php else: ?>
-                    <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
-                        <div class="card-header">
-                            <h6>Conformite Provisions</h6>
-                        </div>
-
-                        <div class="card-body">
-                            <p>Incorrecte</p>
-                        </div>
-                    </div>
-                <?php endif ?>
-            <?php endforeach ?>
-        <?php endforeach ?>
-
-
-        <?php if(array_sum($comptes_complet_debit_solde) >= array_sum($comptes_complet_credit_solde)): ?>
-            <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-                <div class="card-header">
-                    <h6>Conformite VNC</h6>
-                </div>
-
-                <div class="card-body">
-                    <p><?= array_sum($comptes_complet_debit_solde) ?> est <?= ' >= ' ?> <?= array_sum($comptes_complet_credit_solde) ?></p>
-                </div>
+        <div class="card mb-3" style="max-width: 18rem;">
+            <div class="card-header">
+                <h6>Conformite Amortissements</h6>
             </div>
-        <?php else: ?>
-            <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
-                <div class="card-header">
-                    <h6>Conformite VNC</h6>
-                </div>
-
-                <div class="card-body">
-                    <p><?= array_sum($comptes_complet_debit_solde) ?> est <?= ' <= ' ?> <?= array_sum($comptes_complet_credit_solde) ?></p>
-                </div>
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <?php foreach($comptes_complet_debit_mou as $compte_complet_debit_mou): ?>
+                        <?php foreach($comptes_complet_credit_mou as $compte_complet_credit_mou): ?>
+                            <?php if($compte_complet_debit_mou === $compte_complet_credit_mou): ?>
+                                    <li class="list-group-item bg-success text-white">
+                                        <?= $compte_complet_debit_mou ?> est <?= ' = a ' ?> <?= $compte_complet_credit_mou ?>
+                                    </li>
+                            <?php else: ?>
+                                    <li class="list-group-item bg-danger text-white">
+                                        <?= $compte_complet_debit_mou ?> est <?= ' != de ' ?> <?= $compte_complet_credit_mou ?>
+                                    </li>
+                            <?php endif ?>
+                        <?php endforeach ?>
+                    <?php endforeach ?>
+                </ul>
             </div>
-        <?php endif ?>
+        </div>
+
+        <div class="card mb-3" style="max-width: 18rem;">
+            <div class="card-header">
+                <h6>Conformite Provisions</h6>
+            </div>
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <?php foreach($comptes_complet_debit_mou_69 as $compte_complet_debit_mou): ?>
+                        <?php foreach($comptes_complet_credit_mou_29 as $compte_complet_credit_mou): ?>
+                            <?php if($compte_complet_debit_mou === $compte_complet_credit_mou): ?>
+                                    <li class="list-group-item bg-success text-white">
+                                        <?= $compte_complet_debit_mou ?> est <?= ' = a ' ?> <?= $compte_complet_credit_mou ?>
+                                    </li>
+                            <?php else: ?>
+                                    <li class="list-group-item bg-danger text-white">
+                                        <?= $compte_complet_debit_mou ?> est <?= ' != de ' ?> <?= $compte_complet_credit_mou ?>
+                                    </li>
+                            <?php endif ?>
+                        <?php endforeach ?>
+                    <?php endforeach ?>
+                </ul>
+            </div>
+        </div>
+
+        <div class="card mb-3" style="max-width: 18rem;">
+            <div class="card-header">
+                <h6>Conformite VNC</h6>
+            </div>
+            
+            <div class="card-body">
+                <ul class="list-group list-group-flush">
+                    <?php if(array_sum($comptes_complet_debit_solde) >= array_sum($comptes_complet_credit_solde)): ?>
+                        <li class="list-group-item bg-success text-white">
+                            <?= array_sum($comptes_complet_debit_solde) ?> est <?= ' >= a ' ?> <?= array_sum($comptes_complet_credit_solde) ?>
+                        </li>
+                    <?php else: ?>
+                        <li class="list-group-item bg-danger text-white">
+                            <?= array_sum($comptes_complet_debit_solde) ?> est <?= ' <= a ' ?> <?= array_sum($comptes_complet_credit_solde) ?>
+                        </li>
+                    <?php endif ?>
+                </ul>
+            </div>
+        </div>
     </div>
 </div>
 
